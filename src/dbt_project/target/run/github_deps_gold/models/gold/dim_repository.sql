62e31e7b40f0d9a3912e6219faffd3c6e632c669
julien <julien.crm@gmail.com>
2026-06-17 09:09:26 +0200

    modif DBT

--- src/dbt_project/target/run/github_deps_gold/models/gold/dim_repository.sql
+++ src/dbt_project/target/run/github_deps_gold/models/gold/dim_repository.sql
@@ -10,10 +10,10 @@
         
   
 
-  insert into `default`.`dim_repository__dbt_backup`
+  insert into `gold`.`dim_repository__dbt_backup`
         ("repo_id", "repo_name", "github_url")
 
--- Repos are identified as parents in dependency_relations (they have dependents but are not dependencies themselves)
+-- A true repo is a root node: it appears as parent_id but never as child_id
 SELECT DISTINCT
     toUUID(rel.parent_id) as repo_id,
     c.name as repo_name,
@@ -21,4 +21,9 @@ SELECT DISTINCT
 FROM `default`.`silver_dependency_relations` rel
 JOIN `default`.`silver_components` c ON toString(c.id) = rel.parent_id
 WHERE c.github_url IS NOT NULL
+  AND rel.parent_id NOT IN (
+      SELECT DISTINCT child_id
+      FROM `default`.`silver_dependency_relations`
+      WHERE child_id IS NOT NULL
+  )
   
