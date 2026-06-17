fed8f9eb67b75941018829358dedf42e71d43664
Romain-Data <184205713+Romain-Data@users.noreply.github.com>
2026-06-17 12:23:46 +0200

    fix .env route

--- src/monitoring-dashboard/src/lib/clickhouse.ts
+++ src/monitoring-dashboard/src/lib/clickhouse.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@clickhouse/client';
 
 export const clickhouse = createClient({
-  url: `http://${process.env.CLICKHOUSE_HOST}:${process.env.CLICKHOUSE_PORT || '8123'}`,
-  username: process.env.CLICKHOUSE_USER,
-  password: process.env.CLICKHOUSE_PASSWORD,
-  database: process.env.CLICKHOUSE_DATABASE,
+  url: `http://${process.env.DBT_CH_HOST}:${process.env.DBT_CH_PORT || '8123'}`,
+  username: process.env.DBT_CH_USER,
+  password: process.env.DBT_CH_PASSWORD,
+  database: process.env.DBT_CH_DB_SILVER || 'default',
   // On camoufle la requête pour contourner FortiGuard
   http_headers: {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
